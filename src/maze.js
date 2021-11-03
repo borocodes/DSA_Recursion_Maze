39e3ec03efa61479018cabe3223160beed2aeb31
Jonathan Borofsky <jonborofsky@gmail.com>
2021-11-03 14:51:55 -0500

    ran through prettierjs

--- src/maze.js
+++ src/maze.js
@@ -7,34 +7,36 @@
 // My Solution
 function mazeSolver(maze, index = [0, 0]) {
   //create rows and columns for readability
-  let row = index[0] // = first array
-  let col = index[1] // = first element
-  
+  let row = index[0]; // = first array
+  let col = index[1]; // = first element
+
   ///base case
-  if (maze[row][col] === 'e') {return ""}
-  
+  if (maze[row][col] === "e") {
+    return "";
+  }
+
   //mark a cell as visited so we cannot reviist
   maze[row][col] = "*";
-  
+
   //recursive phase
-    //col +1 moves to next element(R), col -1 moves to previous element(L)
-  if(maze[row][col + 1] && maze[row][col +1] !== '*') {
+  //col +1 moves to next element(R), col -1 moves to previous element(L)
+  if (maze[row][col + 1] && maze[row][col + 1] !== "*") {
     col++;
-    return "R" + mazeSolver(maze, [row, col])
+    return "R" + mazeSolver(maze, [row, col]);
   }
-  if(maze[row][col - 1] && maze[row][col -1] !== '*') {
+  if (maze[row][col - 1] && maze[row][col - 1] !== "*") {
     col--;
-    return "L" + mazeSolver(maze, [row, col])
+    return "L" + mazeSolver(maze, [row, col]);
   }
-  
+
   //row +1 moves to next array(D), row -1 moves to previous array(U)
-  if(maze[row + 1] && maze[row +1][col] !== '*') {
+  if (maze[row + 1] && maze[row + 1][col] !== "*") {
     row++;
-    return "D" + mazeSolver(maze, [row, col])
+    return "D" + mazeSolver(maze, [row, col]);
   }
-  if(maze[row -1] && maze[row -1][col] !== '*') {
+  if (maze[row - 1] && maze[row - 1][col] !== "*") {
     row--;
-    return "U" + mazeSolver(maze, [row, col])
+    return "U" + mazeSolver(maze, [row, col]);
   }
 }
 
